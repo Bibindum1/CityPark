955f4b046e360be7b4844dbcbbd1ba7a79f43c37
Bibindum1 <aleksandrsluginov77@gmail.com>
2026-06-24 14:33:58 +0300

    корзина

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('about/', views.about_view, name='about'),
     path('wine/', views.wine_view, name='wine'),
     path('contacts/', views.contacts_view, name='contacts'),
+    path('cart/add/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_view, name='cart'),
+    path('fav/toggle/<int:id>/', views.toggle_fav, name='toggle_fav'),
 ]
